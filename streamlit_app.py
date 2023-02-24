a42cdc552ef73358351e206d3c391f28d13c40ca
kode2go <brsbin001@myuct.ac.za>
2023-02-25 01:04:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,29 +15,19 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+import requests
+import time
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+while True:
+    try:
+        response = requests.get(url, verify=True)
+        if response.status_code == 200:
+            st.write(f"{url} is up!")
+        else:
+            st.write(f"{url} is down (status code {response.status_code})")
+    except requests.exceptions.RequestException as e:
+        st.write(f"{url} is down ({e})")
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    st.write(f"Timestamp: {timestamp}")
+    time.sleep(30)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-    
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
