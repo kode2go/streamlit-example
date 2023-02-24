bdddb54ef527cbc674d3ae1e72fa2d2ebfe83976
kode2go <brsbin001@myuct.ac.za>
2023-02-25 01:16:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,13 @@ while True:
         response = requests.get(url, verify=True)
         if response.status_code == 200:
             status = "up"
-            st.write(f"{url} is up!")
+#             st.write(f"{url} is up!")
         else:
             status = "down"
-            st.write(f"{url} is down (status code {response.status_code})")
+#             st.write(f"{url} is down (status code {response.status_code})")
     except requests.exceptions.RequestException as e:
         status = "down"
-        st.write(f"{url} is down ({e})")
+#         st.write(f"{url} is down ({e})")
         
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
     st.write(f"Timestamp: {timestamp}")
